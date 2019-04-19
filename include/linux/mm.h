2ae154984ef2c60b4612b40df77ecd81077a02ea
Tk-Glitch <ti3nou@gmail.com>
2019-04-19 12:33:38 +0200

    Set vm.max_map_count to 262144 by default
    The value is still pretty low, and AMD64-ABI and ELF extended numbering
    supports that, so we should be fine on modern x86 systems.
    
    This fixes crashes in some applications using more than 65535 vmas (also
    affects some windows games running in wine, such as Star Citizen).

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -204,8 +204,7 @@ static inline void __mm_zero_struct_page(struct page *page)
  * not a hard limit any more. Although some userspace tools can be surprised by
  * that.
  */
-#define MAPCOUNT_ELF_CORE_MARGIN	(5)
-#define DEFAULT_MAX_MAP_COUNT	(USHRT_MAX - MAPCOUNT_ELF_CORE_MARGIN)
+#define DEFAULT_MAX_MAP_COUNT	(262144)
 
 extern unsigned long sysctl_user_reserve_kbytes;
 extern unsigned long sysctl_admin_reserve_kbytes;
